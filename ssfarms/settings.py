ec64c06115c3f6b590b575b09f0d0f79f08a4c17
Ram Charan <ramcharan0127@example.com>
2026-07-25 18:03:59 +0530

    final changes

--- ssfarms/settings.py
+++ ssfarms/settings.py
@@ -120,11 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # --------------------------------------------------
 # 2️⃣ WHATSAPP NUMBER (from env)
 # --------------------------------------------------
