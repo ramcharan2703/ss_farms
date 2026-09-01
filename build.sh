98ad8e94528ba2e7a3b8a4fe16c402541072537b
Ram Charan <ramcharan0127@example.com>
2026-09-01 14:16:54 +0530

    Update production database migration

--- build.sh
+++ build.sh
@@ -2,7 +2,32 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='ramcharan').exists() or User.objects.create_superuser('ramcharan','ramcharan0127@gmail.com','Charan@123')"
+python manage.py collectstatic --noinput
+
+python manage.py shell -c "
+import os
+from django.contrib.auth.models import User
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={'email': email or ''}
+    )
+
+    if created:
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+    else:
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+"
